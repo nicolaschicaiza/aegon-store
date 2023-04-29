45fae10fad54e3da06a875ed5558e177480440dd
jnicolaschc <jefryn@unicauca.edu.co>
2023-04-29 10:15:38 -0500

    :construction: (Basic Form) Trabajando en conceptos básicos de formularios reactivos

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-basic-form',
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./basic-form.component.scss'],
 })
 export class BasicFormComponent implements OnInit {
+    nameField = new FormControl('', [
+        Validators.required,
+        Validators.maxLength(10),
+    ]);
+    emailField = new FormControl('');
+    phoneField = new FormControl('');
+    colorField = new FormControl('#000000');
+    dateField = new FormControl('');
+    ageField = new FormControl(12);
+
+    categoryField = new FormControl('category-2');
+    tagField = new FormControl('');
+
+    agreeField = new FormControl(false);
+
+    genderField = new FormControl('');
+    zoneField = new FormControl('');
+
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.nameField.valueChanges.subscribe((value) => {
+            console.log(value);
+        });
+    }
+
+    getNameValue() {
+        console.log(this.nameField.value);
+    }
 }
